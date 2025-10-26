53926ec2c9b10748be5aab3aa342c3c36decdc21
Ritikgupta-tech <ritik95061@gmail.com>
2025-10-26 23:52:54 +0530

    updating something

--- CHessboard.java
+++ CHessboard.java
@@ -1,4 +1,29 @@
 public class CHessboard{
+    public static boolean isSafe(char board[][],int row,int col){
+        //verticle up 
+        for (int i =row-1; i >=0; i--) {
+            if (board[i][col]=='Q'){
+                return false;
+            }
+            
+        }
+        //diag left up 
+        for (int i =row-1, j=col-1 ; i>=0 && j>=0; i--,j--) {
+            if(board[i][j]=='Q'){
+                return false;
+            }
+            
+        }
+        // diag right up
+        for (int i =row-1,j=col+1; i>=0 && j<board.length; i--,j++) {
+            if (board[i][j]=='Q') {
+                return false;
+                
+            }
+            
+        }
+        return true;
+    }
     public static void nQueens(char board[][],int row){
         // base case 
         if (row==board.length) {
@@ -9,6 +34,13 @@ public class CHessboard{
         
         // column loop
         for (int j = 0; j <board.length; j++) {
+            if (isSafe(board,row,j)) {
+                board[row][j]='Q';
+                nQueens(board, row+1); //function call 
+                board[row][j]='.';  //backtracking step
+            
+                
+            }
             board[row][j]='Q';
             nQueens(board, row+1); //function call 
             board[row][j]='.';  //backtracking step
